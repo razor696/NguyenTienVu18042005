11d77e3ba53d4c64e89bc9201bb387aa65221c6a
Rainnie <nguyensmaison@gmail.com>
2023-08-18 13:07:21 +0700

    Create a++.cs

---
+++ a++.cs
@@ -0,0 +1,15 @@
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            while (true)
+            {
+                a++;
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
