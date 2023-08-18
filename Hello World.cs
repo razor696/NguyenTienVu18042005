ab8fc2966800bea033bc12ae85af9997ae8cb544
Rainnie <nguyensmaison@gmail.com>
2023-08-18 13:05:44 +0700

    Create Hello World.cs

---
+++ Hello World.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lmao 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
