f28cfb41e396955a10f4ad68aec55ed6aa4047f1
Rainnie <nguyensmaison@gmail.com>
2023-08-18 13:34:57 +0700

    Create Operator.cs

---
+++ Operator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UsingOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            Console.WriteLine("Area is: " + area);
+        }
+    }
+}
