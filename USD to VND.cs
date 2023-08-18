d9e386fe2ea8a45b9d1f956b22ea6a2b2d3caff3
Rainnie <nguyensmaison@gmail.com>
2023-08-18 14:00:21 +0700

    Create USD to VND.cs

---
+++ USD to VND.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lmao 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("USD :");
+            float USDf = float.Parse(Console.ReadLine());
+            float VNDf = 23000*USDf;
+            Console.WriteLine("VND :" + VNDf);
+        }
+    }
+}
