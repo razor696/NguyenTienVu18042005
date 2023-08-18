f80532eb42934a00cc7308aa77ed55063d675998
Rainnie <nguyensmaison@gmail.com>
2023-08-18 13:44:01 +0700

    Create HelloMyName.cs

---
+++ HelloMyName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lmao 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name :");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Hello :" + yourName);
+        }
+    }
+}
