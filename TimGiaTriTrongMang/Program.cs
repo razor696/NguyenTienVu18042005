c7f13550ca93c64a0892d39e963edc3c78aa3add
Rainnie <nguyensmaison@gmail.com>
2023-09-08 14:52:08 +0700

    Create Program.cs

---
+++ TimGiaTriTrongMang/Program.cs
@@ -0,0 +1,28 @@
+﻿﻿using System;
+
+namespace TimGiaTri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = { "Messi", "Ronaldo", "Neymar", "Lewandoski", "Benzema", "Rooney", "DeGea", "Beckham", };
+            Console.WriteLine("Enter a name’s student:");
+            string input_name = Console.ReadLine();
+            bool isExist = false;
+            for (int i = 0; i < students.Length; i++)
+            {
+                if (students[i].Equals(input_name))
+                {
+                    Console.WriteLine("Position of the students in the list " + input_name + " is: " + (i + 1));
+                    isExist = true;
+                    break;
+                }
+            }
+            if (!isExist)
+            {
+                Console.WriteLine("Not found" + input_name + " in the list.");
+            }
+        }
+    }
+}
